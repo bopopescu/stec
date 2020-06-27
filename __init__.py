6b7e68fe2f9fdd8e31843e732ee445628fd35441
aminat ajagbe <ajagbeaminat95@gmail.com>
2020-06-27 22:19:42 +0100

    add python file

---
+++ __init__.py
@@ -0,0 +1,13 @@
+"""Initializaing the application instance."""
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    """Render the index/home page."""
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
