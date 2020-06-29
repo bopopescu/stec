731c69711cca6075dc76f85e3d4efa3d285f9b1e
aminat ajagbe <ajagbeaminat95@gmail.com>
2020-06-29 03:55:46 +0100

    add login view function

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,7 @@
 """Initializaing the application instance."""
-from flask import render_template
+from flask import render_template, flash, redirect, url_for
 from app import app
+from app.forms import LoginForm
 
 
 @app.route('/')
@@ -8,3 +9,13 @@ from app import app
 def index():
     """Render the index/home page."""
     return render_template('index.html', title='Home')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    """Render the Sign in page."""
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login successful for user {}'.format(form.username.data))
+        return redirect(url_for('index'))
+    return render_template('login.html', title='Sign in', form=form)
