77250cf67277a82581cbb96b8bd98652bdb049f5
aminat ajagbe <ajagbeaminat95@gmail.com>
2020-06-29 08:06:41 +0100

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,22 @@
 """Initializaing the application instance."""
 from flask import Flask
+import mysql.connector
+import hashlib
 from config import Config
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config.from_object(Config)
+login = LoginManager(app)
+dbconfig = {
+    'user': 'root',
+    'password': 'root',
+    'host': 'localhost',
+    'port': '8889',
+    'raise_on_warnings': True,
+    'database': 'STEC'
+}
+dblink = mysql.connector.connect(**dbconfig)
+dblink.close()
 
 from app import routes
