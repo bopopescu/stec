3d9893f4f4800dab7099440988779878ef81764c
aminat ajagbe <ajagbeaminat95@gmail.com>
2020-06-29 20:14:18 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,12 @@
 """Importing os."""
 import os
+import pymysql
+import dbinfo
+basedir = os.path.abspath(os.path.dirname(__file__))
 
+dblink = 'mysql+pymysql://{0}:{1}@{2}/{3}'.format(dbinfo.user, dbinfo.dbpass, dbinfo.host, dbinfo.dbname)
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'stec-web-developed'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or dblink
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
