fab6286ce1c9cf8a515aa5b29cb899f0691a6db4
aminat ajagbe <ajagbeaminat95@gmail.com>
2020-07-03 21:58:09 +0100

    add errors file

---
+++ app/errors.py
@@ -0,0 +1,16 @@
+"""Initializaing the application instance."""
+from flask import render_template
+from app import app, db
+
+
+@app.errorhandler(404)
+def not_found_error(error):
+    """Render the page for error 404."""
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def internal_error(error):
+    """Render the page for error 500."""
+    db.session.rollback()
+    return render_template('500.html'), 500
