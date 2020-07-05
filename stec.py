4bc26b001b1a26d66d41a976ccc2ae9cc172571f
aminat ajagbe <ajagbeaminat95@gmail.com>
2020-07-05 02:42:54 +0100

    update commit

--- stec.py
+++ stec.py
@@ -1,13 +1,14 @@
 """Initializaing the application instance."""
 from app import app, db
-from app.models import Users, Posts
+from app.models import Users, Posts, UserPosts, Contacts
 
 
 @app.shell_context_processor
 def make_shell_context():
     """Database tables."""
-    return {'db': db, 'Users': Users, 'Posts': Posts}
+    return {'db': db, 'Users': Users, 'Posts': Posts, 'UserPosts': UserPosts,
+            'Contacts': Contacts}
 
 
-# if __name__ == '__main__':
-#     app.run(debug=True)
+if __name__ == '__main__':
+    app.run(debug=False)
