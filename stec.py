51ea77a30224d67d7b64c3ec2b0b767018af8681
aminat ajagbe <ajagbeaminat95@gmail.com>
2020-07-11 23:36:23 +0100

    add unittest

--- stec.py
+++ stec.py
@@ -11,5 +11,14 @@ def make_shell_context():
                 Contacts=Contacts, UserMessages=UserMessages,
                 Notifications=Notifications)
 
+
+@app.cli.command()
+def test():
+    """Run the unit tests."""
+    import unittest
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
+
+
 if __name__ == '__main__':
     app.run()
--- tests.py
+++ tests.py
@@ -1,11 +1,11 @@
 """Initializaing the application instance."""
 from datetime import datetime, timedelta
 import unittest
+from flask import current_app
 from app import app, db
-from app.models import Users, UserPosts
 
 
-class UserModelCase(unittest.TestCase):
+class StecTestCase(unittest.TestCase):
     """Testing different functions."""
 
     def setUp(self):
@@ -18,43 +18,10 @@ class UserModelCase(unittest.TestCase):
         db.session.remove()
         db.drop_all()
 
-    def test_password_hashing(self):
-        """Testing password hash."""
-        u = Users(Username='aminat')
-        u.set_password('tests')
-        self.assertFalse(u.check_password('testsfalse'))
-        self.assertTrue(u.check_password('tests'))
+    def test_app_exists(self):
+        """Testing the application instance exists."""
+        self.assertFalse(current_app is None)
 
-    def test_users(self):
-        """Testing users."""
-        user = Users(Name='john doe', Username='john', Email='john@examp.com')
-        db.session.add(user)
-        db.session.commit()
-
-    def test_userPosts(self):
-        """Testing user post."""
-        # create four users
-        u1 = Users(Name='john doe', Username='john', Email='john@example.com')
-        u2 = Users(Name='susan wilson', Username='susan',
-                   Email='susan@example.com')
-        u3 = Users(Name='mary jane', Username='mary', Email='mary@example.com')
-        u4 = Users(Name='david williams', Username='david',
-                   Email='david@example.com')
-        db.session.add_all([u1, u2, u3, u4])
-
-        # create four posts
-        now = datetime.utcnow()
-        p1 = UserPosts(Body="post from john", author=u1,
-                       Timestamp=now + timedelta(seconds=1))
-        p2 = UserPosts(Body="post from susan", author=u2,
-                       Timestamp=now + timedelta(seconds=4))
-        p3 = UserPosts(Body="post from mary", author=u3,
-                       Timestamp=now + timedelta(seconds=3))
-        p4 = UserPosts(Body="post from david", author=u4,
-                       Timestamp=now + timedelta(seconds=2))
-        db.session.add_all([p1, p2, p3, p4])
-        db.session.commit()
-
-
-if __name__ == '__main__':
-    unittest.main(verbosity=2)
+    def test_app_is_testing(self):
+        """Testing the instance is running."""
+        self.assertTrue(current_app)
