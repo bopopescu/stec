362ae6f1ea1ca7d3316100d557cf101524dcfd3f
aminat ajagbe <ajagbeaminat95@gmail.com>
2020-07-12 05:09:50 +0100

    update commit

--- tests.py
+++ tests.py
@@ -1,8 +1,9 @@
 """Initializaing the application instance."""
+from datetime import datetime, timedelta
 import unittest
 from flask import current_app
 from app import app, db
-from app.models import Users
+from app.models import Users, UserPosts
 
 
 class StecTestCase(unittest.TestCase):
@@ -50,3 +51,31 @@ class UsersModelTestCase(unittest.TestCase):
         user1.set_password('test')
         user2.set_password('test')
         self.assertTrue(user1.Password != user2.Password)
+
+    def test_users(self):
+        """Testing Users table."""
+        user = Users(Name='John Doe',
+                     Username='johnd',
+                     Email='john@examp.com')
+        user.set_password('john')
+        db.session.add(user)
+        db.session.commit()
+
+class UserPostsModelTestCase(unittest.TestCase):
+    """Testing the UserPosts Model."""
+
+    def test_userposts(self):
+        user1 = Users(Name='john doe',
+                      Username='johnd',
+                      Email='john@example.com')
+        user2 = Users(Name='susan wilson',
+                      Username='susan',
+                      Email='susan@example.com')
+        db.session.add_all([post1, post2])
+        now = datetime.utcnow()
+        post1 = UserPosts(Body="post from john", author=user1,
+                          Timestamp=now + timedelta(seconds=1))
+        post2 = UserPosts(Body="post from susan", author=user2,
+                          Timestamp=now + timedelta(seconds=4))
+        db.session.add_all([post1, post2])
+        db.session.commit()
